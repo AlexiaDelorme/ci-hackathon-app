947b0977dbce7ce1e984e7efc0e988925551030e
Alexia Delorme <alexia.delorme@gmail.com>
2020-11-03 22:00:42 +0000

    #S02 & #P02 Overid default all-auth PasswordChangeView class success url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.decorators import login_required
+from allauth.account.views import PasswordChangeView
 from . import views
 
 urlpatterns = [
     path("edit_profile/", views.edit_profile, name="edit_profile"),
+    # Override django allauth default success url
+    path('password/change/',
+         login_required(
+             PasswordChangeView.as_view(
+                 success_url=reverse_lazy('profile')
+             )
+         ),
+         name='account_change_password'
+         ),
 ]
